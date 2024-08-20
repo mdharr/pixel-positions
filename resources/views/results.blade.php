95f8185a61d83ca015c3790e326a2fa9c94fd3e9
Michael Harrington <mdharr7@gmail.com>
2024-08-20 17:22:44 +0800

    Only render search results when matching results available

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -4,8 +4,17 @@
     </x-page-heading>
 
     <div class="space-y-6">
-        @foreach($jobs as $job)
-            <x-job-card-wide :$job />
-        @endforeach
+        @if($jobs->isNotEmpty())
+            @foreach($jobs as $job)
+                <x-job-card-wide :$job />
+            @endforeach
+        @else
+            <div class="text-center flex-col space-y-10">
+                <h2 class="text-2xl mt-20">No matching results...</h2>
+                <p class="text-gray hover:text-blue-500">
+                    <a href="/">Return Home</a>
+                </p>
+            </div>
+        @endif
     </div>
 </x-layout>
